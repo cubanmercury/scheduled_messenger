07eb5840d8babc69582a24167ac884c52a7be5a2
Adam <a_martin15@hotmail.com>
2019-03-20 21:14:03 +0000

    fixed date format issue

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
 
 function Timer() {
     var dt=new Date()
-    document.getElementById('time').innerHTML=dt.getHours()+":"+dt.getMinutes()+":"+dt.getSeconds();
+    var minutes = dt.getMinutes();
+    let seconds = dt.getSeconds();
+    
+    if (minutes < 10){
+        minutes = "0" + minutes;
+    }
+    if (seconds < 10){
+        seconds = "0" + seconds;
+    }
+    document.getElementById('time').innerHTML=dt.getHours()+":"+minutes+":"+seconds;
     setTimeout("Timer()",1000);
  }
  Timer();
